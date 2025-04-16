7d3169f148a0944e83cc83527b63866b79acc844
Ahlam <ahlam.ahmed@aastustudent.edu.et>
2025-04-16 10:25:18 +0300

    Updating movie functions

--- main/script.js
+++ main/script.js
@@ -19,6 +19,7 @@
     
                 if(data.results.length > 0){
                     data.results.forEach(movie =>{
+                        if(!movie.poster_path || !movie.title)return;
                         const imageUrl = movie.poster_path 
                         ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
                         : 'https://via.placeholder.com/200x300?text=No+Image';
@@ -41,9 +42,9 @@
             console.error('Error fetching movie:', error);
             }
         }
-        async function fetchLatestmovie(){
+        async function fetchnowplayingmovie(){
             const API_KEY='eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZDc5Nzg1Zjk4OGY4MWU5YWU2MDM1ZjY0ZDA4MmNhNSIsIm5iZiI6MTc0MzU5Njc0My45NTksInN1YiI6IjY3ZWQyY2M3MGQ3MTIxYTljYzAxMzk4MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VOC55mDLn4vjYGkdzoN1Dgb6HXZRKqCtagmXyCfLInM';
-            const url=`https://api.themoviedb.org/3/movie/latest`
+            const url=`https://api.themoviedb.org/3/movie/now_playing?language=en-US`
 
         try {
             const res=await fetch(url,{
@@ -55,22 +56,25 @@
             );
 
             const data = await res.json();
-            const container = document.getElementById('Latest-container');
+            const container = document.getElementById('movies-container');
             container.innerHTML = '';
 
-            if(data){
-                const imageUrl = movie.poster_path
-                    ? `https://image.tmdb.org/t/p/w500${data.poster_path}`
-                    : 'https://via.placeholder.com/200x300?text=No+Image';
-                   
+            if(data.results.length > 0){
+                data.results.forEach(movie => {
+                    if(!movie.poster_path || !movie.title) return;
+                    const imageUrl = movie.poster_path
+                        ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                        : 'https://via.placeholder.com/200x300?text=No+Image';
+                    
                     const template=document.getElementById('main-template');
                     const clone= template.content.cloneNode(true);
                     clone.querySelector('.moviePoster').src = imageUrl;
                     clone.querySelector('.moviePoster').alt = movie.title;
                     clone.querySelector('.movieTitle').textContent = movie.title;
                     clone.querySelector('.movieDate').textContent = `Release Date: ${movie.release_date || 'N/A'}`;
-                
-                container.appendChild(clone);
+                    
+                    container.appendChild(clone);
+                });
             
         } else {
             container.innerHTML = '<p>No results found.</p>';
@@ -81,7 +85,6 @@
         }
     async function fetchtopmovie(){
         const API_KEY='eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZDc5Nzg1Zjk4OGY4MWU5YWU2MDM1ZjY0ZDA4MmNhNSIsIm5iZiI6MTc0MzU5Njc0My45NTksInN1YiI6IjY3ZWQyY2M3MGQ3MTIxYTljYzAxMzk4MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VOC55mDLn4vjYGkdzoN1Dgb6HXZRKqCtagmXyCfLInM';
-        const movieName = 'top rated';
         const url=`https://api.themoviedb.org/3/movie/top_rated`
 
     try {
@@ -92,7 +95,6 @@
         }
         }
         );
-
         const data = await res.json();
         const container = document.getElementById('movies-container');
         container.innerHTML = '';
@@ -119,7 +121,53 @@
     console.error('Error fetching movie:', error);
     }
 }
-        document.addEventListener('DOMContentLoaded', fetchLatestmovie);
-        document.addEventListener('DOMContentLoaded', fetchtopmovie);
+
+async function fetchupcomingmovie(){
+    const API_KEY='eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZDc5Nzg1Zjk4OGY4MWU5YWU2MDM1ZjY0ZDA4MmNhNSIsIm5iZiI6MTc0MzU5Njc0My45NTksInN1YiI6IjY3ZWQyY2M3MGQ3MTIxYTljYzAxMzk4MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VOC55mDLn4vjYGkdzoN1Dgb6HXZRKqCtagmXyCfLInM';
+    const url=`https://api.themoviedb.org/3/movie/upcoming`
+    
+
+try {
+    const res=await fetch(url,{
+    headers:{
+        Authorization:`Bearer ${API_KEY}`,
+        'Content-Type':'application/json;charset=utf-8'
+    }
+    }
+    );
+
+    const data = await res.json();
+    const container = document.getElementById('movies-container');
+    container.innerHTML = '<h3>Upcoming</h3>';
+
+
+    if(data.results.length > 0){
+        data.results.forEach(movie => {
+            if(!movie.poster_path || !movie.title) return;
+            const imageUrl = movie.poster_path
+                ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                : 'https://via.placeholder.com/200x300?text=No+Image';
+            
+            const template=document.getElementById('main-template');
+            const clone= template.content.cloneNode(true);
+            clone.querySelector('.moviePoster').src = imageUrl;
+            clone.querySelector('.moviePoster').alt = movie.title;
+            clone.querySelector('.movieTitle').textContent = movie.title;
+            clone.querySelector('.movieDate').textContent = `Release Date: ${movie.release_date || 'N/A'}`;
+            
+            container.appendChild(clone);
+        });
+    
+} else {
+    container.innerHTML = '<p>No results found.</p>';
+}
+}  catch (error) {
+console.error('Error fetching movie:', error);
+}
+}
+        document.addEventListener('DOMContentLoaded', fetchupcomingmovie);
         document.getElementById('search-button').addEventListener('click', fetchmovie);
+        document.getElementById('toprated-button').addEventListener('click', fetchtopmovie);
+        document.getElementById('nowplaying-button').addEventListener('click', fetchnowplayingmovie);
+    
     
